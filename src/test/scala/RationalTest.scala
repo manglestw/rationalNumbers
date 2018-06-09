de7a7d4a72325b230424b428fde7076cd0d1efcb
manglestw <mangles@thoughtworks.com>
2018-06-09 12:02:29 +0200

    Mireia - Added more tests to check that gcd is working with all operation

--- src/test/scala/RationalTest.scala
+++ src/test/scala/RationalTest.scala
@@ -14,15 +14,28 @@ class RationalTest extends FunSuite with Matchers {
     val result = new Rational(1, 2).add(new Rational(1, 3))
     result should be(new Rational(5, 6))
   }
+  test("should add two rational numbers and return the normalized one"){
+    val result = new Rational(1, 6).add(new Rational(2, 3))
+    result.toString should be("5/6")
+  }
 
   test("should substract two rational numbers") {
     val result = new Rational(1, 2).substract(new Rational(2, 3))
     result should be(new Rational(-1, 6))
   }
 
+  test("should substract two rational numbers and return the normalized one") {
+    val result = new Rational(1, 6).substract(new Rational(2, 3))
+    result.toString should be("-1/2")
+  }
+
   test("should multiply two rational numbers") {
     val result = new Rational(1, 2).multiply(new Rational(2, 3))
     result should be(new Rational(2, 6))
+  }
+  test("should multiply two rational numbers and return the normalized one") {
+    val result = new Rational(5, 6).multiply(new Rational(2, 3))
+    result.toString should be("5/9")
   }
 
   test("should divide two rational numbers") {
@@ -30,8 +43,14 @@ class RationalTest extends FunSuite with Matchers {
     result should be(new Rational(3, 4))
   }
 
+  test("should divide two rational numbers and return the normalized one") {
+    val result = new Rational(1, 6).divide(new Rational(2, 3))
+    result.toString should be("1/4")
+  }
+
   test("should return an equivalent rational number normalized by greatest common divisor") {
-    new Rational(10, 20) should be(new Rational(1, 2))
+    val rational = new Rational(10, 20)
+    rational.toString should be("1/2")
   }
 
 }
